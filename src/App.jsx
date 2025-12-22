b56813271ec15aef7243e3eadbbe04d3addfb10b
ian-michael254 <ian.michael.codes@gmail.com>
2025-12-22 21:36:12 +0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div  className='bg-pink-500 text-bold text-center underline text-violet-800 text-6xl'>
+      PATRICK OCHIENG
+    </div>
+  )
+}
+
+export default App
