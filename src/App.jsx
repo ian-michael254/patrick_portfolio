3c893477ea4e915fa5ab508e73a286381246c7ca
ian-michael254 <ian.michael.codes@gmail.com>
2025-12-24 22:56:33 +0300

    components creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import About from './components/about.jsx'
+import Books from './components/Books.jsx'
+import Testimonials from './components/Testimonials.jsx'
 
 
 function App() {
   return (
-   <div className='w-full mx-w-full h-auto mx-h-auto bg-pink-300 overflow-x-hidden overflow-y-hidden px-10 py-5 mx-auto font-body box-border' >
+   <div className='w-full mx-w-full h-auto mx-h-auto bg-pink-300 overflow-x-hidden overflow-y-hidden px-4 py-2 mx-auto font-body box-border' >
     <Navbar />
+    <Hero />
+    <About />
+    <Books />
+    <Testimonials />
      
     
    </div>
